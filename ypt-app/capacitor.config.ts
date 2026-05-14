821ede83c87b09b96f6a809792d25f4ec3f05c27
Marlon Lewellyn Horn <149269855+marlonhorn@users.noreply.github.com>
2026-05-14 16:39:57 +0000

    chore: scaffold ionic-angular app with capacitor

---
+++ ypt-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.marlonhorn.youtubeperformancetracker',
+  appName: 'YouTube Performance Tracker',
+  webDir: 'www'
+};
+
+export default config;
